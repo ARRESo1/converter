c44bebdc6fc99fa65d1504b436dc5febc764140d
Badriddin <frozeshkh@gmail.com>
2022-08-19 21:29:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+window.onload = async () => {
+    const converter = {
+        inputs: document.querySelectorAll(".converter__input"),
+        selects: document.querySelectorAll(".converter-select"),
+        currentItemTexts: document.querySelectorAll(".converter-select__current-text"),
+        itemBoxes: document.querySelectorAll(".converter-select__item-box")
+    };
+
+    const selectedCurrencies = ["RUB", "USD"];
+
+    const requestTypes = ["USD", "EUR", "KZT", "INR", "KRW"];
+    const currencies = await Currency.requestValues(requestTypes);
+
+    const convertToRUB = (typeCurrency, value) => currencies[typeCurrency] * value;
+    const onChangeInput = (index, value) => {
+        const _index = +!index;
+        const convertedValue = convertToRUB(selectedCurrencies[index], value) / currencies[selectedCurrencies[_index]];
+
+        converter.inputs[_index].value = parseFloat(convertedValue.toFixed(2));
+    };
+
+    converter.itemBoxes.forEach((element, index) => {
+        converter.currentItemTexts[index].innerHTML = selectedCurrencies[index];
+
+        for (const key in currencies) {
+            if (Object.hasOwnProperty.call(currencies, key)) {
+                const newItem = document.createElement("li");
+
+                newItem.classList.add("converter-select__item");
+                newItem.innerHTML = key;
+
+                element.appendChild(newItem);
+            }
+        }
+    });
+
+    converter.selects.forEach((element, index) => {
+        element.addEventListener("click", event => {
+            const target = event.target;
+            const currentItemText = converter.currentItemTexts[index];
+
+            if (target.classList.value === "converter-select__item" && selectedCurrencies[index] != target.innerHTML) {
+                selectedCurrencies[index] = target.innerHTML;
+                currentItemText.innerHTML = target.innerHTML;
+
+                onChangeInput(index, converter.inputs[index].value);
+            }
+
+            element.classList.toggle("converter-select_actived");
+        });
+    });
+
+    converter.inputs.forEach((element, index) => {
+        element.removeAttribute("disabled");
+
+        element.addEventListener("input", event => {
+            const value = +event.target.value;
+
+            onChangeInput(index, value);
+        });
+    });
+};
---
+++ script1.js
@@ -0,0 +1,47 @@
+class Currency {
+    static url = "https://www.cbr-xml-daily.ru/latest.js";
+
+    static async startLoading() {
+        document.querySelector(".converter__loading").setAttribute("style", "display: block");
+    }
+
+    static async stopLoading() {
+        document.querySelector(".converter__loading").setAttribute("style", "display: none");
+    }
+
+    static async requestValues(requestTypes) {
+        this.startLoading();
+
+        try {
+            const data = await fetch("https://www.cbr-xml-daily.ru/latest.js");
+            const { rates } = await data.json();
+
+            const response = { "RUB": 1 };
+
+            requestTypes.forEach(type => {
+                const currecnyInRUB = 1 / rates[type];
+
+                if (type in rates) response[type] = parseFloat(currecnyInRUB.toFixed(2));
+                else response[type] = 0;
+            });
+
+
+            this.stopLoading();
+
+            return response;
+        }
+        catch (error) {
+            console.error(error);
+
+            this.stopLoading();
+
+            return {
+                "RUB": 1,
+                "USD": 105.81,
+                "EUR": 115.62,
+                "KZT": 0.20,
+                "INR": 1.38,
+            };
+        }
+    }
+}   
